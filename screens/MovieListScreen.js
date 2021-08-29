96bf4d8152785541ac4d5ebef647618a0265385f
kacperfleming <kfleming424@gmail.com>
2021-08-29 18:44:11 +0200

    searching for movies and displaying matches as a list provided

--- screens/MovieListScreen.js
+++ screens/MovieListScreen.js
@@ -1,9 +1,71 @@
 import React, {useState, useEffect} from 'react';
-import {ViewBase, FlatList, StyleSheet} from 'react-native';
+import {View, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
 import {SearchBar, LinearProgress} from 'react-native-elements';
 
 import MovieCard from '../components/MovieCard';
 
-export default function MovieListScreen() {
-  return null;
+export default function MovieListScreen({navigation}) {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const [searchVal, setSearchVal] = useState();
+
+  useEffect(() => {
+    if (!searchVal | isLoading) return;
+
+    let timeout;
+
+    timeout = setTimeout(async () => {
+      setIsLoading(true);
+
+      try {
+        const movieListData = await (
+          await fetch(
+            `https://api.themoviedb.org/3/search/movie?query=${searchVal}&page=1&language=pl-PL&api_key=908c8ee616534e11d253631e8399c456`,
+          )
+        ).json();
+
+        setData(movieListData);
+      } catch (err) {
+        setError(err);
+      }
+
+      setIsLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [searchVal]);
+
+  return (
+    <View style={styles.root}>
+      <SearchBar value={searchVal} onChangeText={val => setSearchVal(val)} />
+      {isLoading && <LinearProgress />}
+      {data && !isLoading && !error && (
+        <FlatList
+          data={data}
+          renderItem={({item}) => (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('Movie Details', {title: item.title})
+              }>
+              <MovieCard
+                title={item.title}
+                posterPath={item.poster_path}
+                popularity={item.popularity}
+                voteCount={item.vote_count}
+              />
+            </TouchableOpacity>
+          )}
+        />
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+});
